2071ac4e9c3f1a41c08da50d093470fbe517d3a5
Utkarsh <unofficialutkarsh.06@gmail.com>
2025-12-30 21:07:35 +0530

    Add .gitignore and update project files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Utkarsh Kumar",
+    author_email="unofficialutkarsh.06@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+    
+    
+    
+)
